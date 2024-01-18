1b2fee65e00aa606d5c6464835ccf624cb81e0b8
Sreejita-code <chakrabortynina78@gmail.com>
2024-01-18 20:03:17 +0530

    6Companies30DaysChallenge

---
+++ Adobe/LC-2745.java
@@ -0,0 +1,11 @@
+package Adobe;
+
+class Solution {
+    public int longestString(int x, int y, int z) {
+       int a=2*Math.min(x,y);
+       if(x!=y){
+           a++;
+       } 
+       return 2*(a+z);
+    }
+}
